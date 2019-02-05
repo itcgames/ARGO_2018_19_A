78d9b58bc880ded4b0a3e8f23ccc0cc313e69795
Rafael Plugge <rafael.plugge@email.com>
2019-02-05 11:40:38 +0000

    Created base classes

---
+++ ArgoProject/src/stateMachines/BaseState.h
@@ -0,0 +1,33 @@
+﻿#ifndef _FINITE_STATE_MACHINES_BASE_STATE_H
+#define _FINITE_STATE_MACHINES_BASE_STATE_H
+
+namespace app::fsm
+{
+	class BaseState
+	{
+	public: // Constructors/Destructor/Assignments
+		BaseState() = default;
+		~BaseState() = default;
+
+		BaseState(BaseState const &) = default;
+		BaseState & operator=(BaseState const &) = default;
+
+		BaseState(BaseState &&) = default;
+		BaseState & operator=(BaseState &&) = default;
+
+	public: // Public Static Functions
+	public: // Public Member Functions
+	public: // Public Static Variables
+	public: // Public Member Variables
+	protected: // Protected Static Functions
+	protected: // Protected Member Functions
+	protected: // Protected Static Variables
+	protected: // Protected Member Variables
+	private: // Private Static Functions
+	private: // Private Member Functions
+	private: // Private Static Variables
+	private: // Private Member Variables
+	};
+}
+
+#endif // !_FINITE_STATE_MACHINES_BASE_STATE_H
