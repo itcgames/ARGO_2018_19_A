9d111aac588305cfa852889d8c635f816c8d674b
JohnCodd <john.codd@hotmail.com>
2019-02-07 15:26:02 +0000

    Made changes to comments

--- ArgoProject/src/factories/scenes/SplashSceneFactory.cpp
+++ ArgoProject/src/factories/scenes/SplashSceneFactory.cpp
@@ -14,7 +14,8 @@ std::vector<app::Entity> app::fact::sce::SplashSceneFactory::create()
 	auto entities = std::vector<app::Entity>();
 
 	auto player = fact::PlayerFactory().create();
-	auto level = fact::LevelFactory().create();
+	auto level = fact::LevelFactory().create();	
+	entities.insert(entities.end(), level.begin(), level.end());
 	entities.push_back(player);
 	entities.push_back(fact::CameraFactory(player).create());
 
