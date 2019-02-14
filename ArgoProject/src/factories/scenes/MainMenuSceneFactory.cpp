ca613f2e6f2ecbe5b99d635372a55909b84f2b6f
Rafael Plugge <rafael.plugge@email.com>
2019-02-14 15:38:26 +0000

    Added SDL Font Support

--- ArgoProject/src/factories/scenes/MainMenuSceneFactory.cpp
+++ ArgoProject/src/factories/scenes/MainMenuSceneFactory.cpp
@@ -11,14 +11,14 @@ std::vector<app::Entity> app::fact::sce::MainMenuSceneFactory::create()
 	{
 		auto const & size = math::Vector2f{ 1366.0f, 768.0f };
 		auto const & origin = size / 2.0f;
-		auto const & position = math::Vector2f{ 0.0f, 0.0f } + origin;
+		auto const & position = math::Vector2f{ 0.0f, 0.0f };
 		auto const & textureKey = app::res::TextureKey::Debug;
 		entities.push_back(fact::BackgroundImageFactory(position, size, origin, textureKey).create());
 	}
 	{
 		auto const & position = math::Vector2f{ 0.0f, 0.0f };
 		auto const & sizePerLetter = math::Vector2f{ 20.0f, 40.0f };
-		auto const & text = std::string("Debug Text");
+		auto const & text = std::string("Are you not entertained!");
 		auto const & stepSize = math::Vector2f{ static_cast<float>(text.size()), 1.0f };
 		entities.push_back(fact::TextFactory(position, sizePerLetter * stepSize, text).create());
 	}
