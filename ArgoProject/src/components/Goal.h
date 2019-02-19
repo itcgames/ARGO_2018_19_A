0c74de21fe5a3d855bf0aae10bbece2461916ae5
JohnCodd <john.codd@hotmail.com>
2019-02-19 10:00:40 +0000

    Initital commit

---
+++ ArgoProject/src/components/Goal.h
@@ -0,0 +1,35 @@
+﻿#ifndef _COMPONENTS_GOAL_H
+#define _COMPONENTS_GOAL_H
+
+namespace app::comp
+{
+	struct Goal
+	{
+	public: // Constructors/Destructor/Assignments
+		Goal() = default;
+		~Goal() = default;
+
+		Goal(Goal const &) = default;
+		Goal & operator=(Goal const &) = default;
+
+		Goal(Goal &&) = default;
+		Goal & operator=(Goal &&) = default;
+
+	public: // Public Static Functions
+	public: // Public Member Functions
+	public: // Public Static Variables
+	public: // Public Member Variables
+
+	protected: // Protected Static Functions
+	protected: // Protected Member Functions
+	protected: // Protected Static Variables
+	protected: // Protected Member Variables
+	private: // Private Static Functions
+	private: // Private Member Functions
+	private: // Private Static Variables
+	private: // Private Member Variables
+	};
+}
+
+#endif // !_COMPONENTS_GOAL_H
+
