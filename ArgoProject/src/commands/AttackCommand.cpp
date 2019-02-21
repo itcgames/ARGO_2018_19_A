ca180697171fa1424e3863cc234231273e5f1cc2
sybek96 <sybek96@interia.pl>
2019-02-21 11:36:28 +0000

    started feature-axe-attack

---
+++ ArgoProject/src/commands/AttackCommand.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "AttackCommand.h"
+#include "components/CharacterType.h"
+
+app::cmnd::AttackCommand::AttackCommand(app::Entity const _entity)
+	: m_entity(_entity)
+{
+}
+
+void app::cmnd::AttackCommand::execute()
+{
+	if (m_registry.has<comp::CharacterType>(m_entity))
+	{
+		auto& typeComp = m_registry.get<comp::CharacterType>(m_entity);
+		const comp::CharacterType::Type & typeEnum = typeComp.m_type;
+
+		switch (typeEnum)
+		{
+		case comp::CharacterType::Type::AXE:
+		{
+			
+		}
+			break;
+		case comp::CharacterType::Type::BOMB:
+			break;
+		case comp::CharacterType::Type::DISC:
+			break;
+		case comp::CharacterType::Type::SWORD_LEGS:
+			break;
+		default:
+			break;
+		}
+	}
+}
--- ArgoProject/src/factories/AIFactory.cpp
+++ ArgoProject/src/factories/AIFactory.cpp
@@ -93,9 +93,10 @@ std::vector<app::Entity> app::fact::AIFactory::create()
 	collision.bounds = cute::c2AABB();
 	m_registry.assign<decltype(collision)>(entity, std::move(collision));
 
-	auto health = comp::Health();
-	health.health = 1;
-	m_registry.assign<decltype(health)>(entity, std::move(health));
+	//TODO: uncomment
+	//auto health = comp::Health();
+	//health.health = 1;
+	//m_registry.assign<decltype(health)>(entity, std::move(health));
 
 	auto player = comp::PlatformDrop();
 	m_registry.assign<decltype(player)>(entity, std::move(player));
