b9e5555330fcb36355065da7cc7a5baa8c189372
David Nolan <davidnln13@yahoo.com>
2019-02-21 17:56:53 +0000

    working on loadingsystem

--- ArgoProject/src/systems/LoadingSystem.cpp
+++ ArgoProject/src/systems/LoadingSystem.cpp
@@ -4,12 +4,16 @@
 // components
 #include "components/Animator.h"
 #include "components/Render.h"
+#include "components/AnimatedImage.h"
 
 void app::sys::LoadingSystem::update(app::time::seconds const & dt)
 {
-	m_registry.view<comp::Animator, comp::Render>()
-		.each([&, this](app::Entity const entity, comp::Animator & animator, comp::Render & render)
+	m_registry.view<comp::Animator, comp::Render, comp::AnimatedImage>()
+		.each([&, this](app::Entity const entity, comp::Animator & animator, comp::Render & render, comp::AnimatedImage & animatedImage)
 	{
-		//if(render.texture == m_resourceManager.getTexture(m_textureKey))
+		if (animator.currentFrame.x == (animator.numOfFrames.x - 1) && animator.currentFrame.y == (animator.numOfFrames.y - 1))
+		{
+			//render.texture = texture
+		}
 	});
 }
