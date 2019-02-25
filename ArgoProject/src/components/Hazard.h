83fcb7e11e3a56beb249da0c0c5035a7085823b1
Rafael Plugge <rafael.plugge@email.com>
2019-02-25 11:44:03 +0000

    Fixed hazards causing enemies to infinite loop.

---
+++ ArgoProject/src/components/Hazard.h
@@ -0,0 +1,33 @@
+﻿#ifndef _COMP_HAZARD_H
+#define _COMP_HAZARD_H
+
+namespace app::comp
+{
+	struct Hazard
+	{
+	public: // Constructors/Destructor/Assignments
+		Hazard() = default;
+		~Hazard() = default;
+
+		Hazard(Hazard const &) = default;
+		Hazard & operator=(Hazard const &) = default;
+
+		Hazard(Hazard &&) = default;
+		Hazard & operator=(Hazard &&) = default;
+
+	public: // Public Static Functions
+	public: // Public Member Functions
+	public: // Public Static Variables
+	public: // Public Member Variables
+	protected: // Protected Static Functions
+	protected: // Protected Member Functions
+	protected: // Protected Static Variables
+	protected: // Protected Member Variables
+	private: // Private Static Functions
+	private: // Private Member Functions
+	private: // Private Static Variables
+	private: // Private Member Variables
+	};
+}
+
+#endif // !_COMP_HAZARD_H
