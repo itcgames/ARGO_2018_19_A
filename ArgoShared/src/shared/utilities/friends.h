d7bbddc320cb1d0cd3404bcc0a94fafa27a9671b
Rafael Plugge <rafael.plugge@email.com>
2019-02-27 12:26:04 +0000

    Removed unused files.

--- ArgoShared/src/shared/utilities/friends.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef _UTILITIES_FRIENDS_FUNCTIONS_H
-#define _UTILITIES_FRIENDS_FUNCTIONS_H
-
-namespace app::util::fri
-{
-	void send(std::map<std::int32_t, std::string> const & map);
-}
-
-#endif // !_UTILITIES_FRIENDS_FUNCTIONS_H
